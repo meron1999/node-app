a774a6bca8a6a8b0f96ee14d6e8cbe2948afb631
OTAKE_PC\zxyv_ <zxyv_fujiww@yahoo.co.jp>
2025-06-04 09:40:34 +0900

    htmlを別ファイルで作成して読み込ませる

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const http =require('http');
+const fs =require('fs');
+
 
 var server =http.createServer(
     (request,response)=>{
-        response.setHeader('Content-Type','text/html');
-        response.write('<!DOCTYPE html><html lang="ja">');
-        response.write('<head><meta charset="utf-8">');
-        response.write('<title>Hello</title>');
-        response.write('</head>');
-        response.write('<body>');
-        response.write('<h1>Hello Node.js!</h1>');
-        response.write('<p>This is my first Node.js server.</p>');
-        response.write('<p>これは私の初めてのnode.jsサーバーです</p>','utf-8');
-        response.write('</body>');
-        response.write('</html>')
+       fs.readFile('./index.html','utf-8',
+        (err,data)=>{
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write(data);
+        response.end();
+        });
     }
 );
 
