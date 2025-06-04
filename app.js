040f138c7de23c1766be9caef6549b06edc2f09e
OTAKE_PC\zxyv_ <zxyv_fujiww@yahoo.co.jp>
2025-06-04 09:17:36 +0900

    サーバー起動時にメッセージが表示されるように修正

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ var server =http.createServer(
     }
 );
 
-server.listen(3000);
+const PORT = 3000; // ポート番号を変数にしておくと便利です
+server.listen(PORT, () => {
+    // サーバーが正常に起動し、ポートの待ち受けを開始したときに実行される
+    console.log(`Server running at http://localhost:${PORT}/`);
+    console.log('サーバーが起動しました。Ctrl+Cで停止できます。');
+});
