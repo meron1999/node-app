f3618d23d88f7910afe8e778fb979181be917ef9
OTAKE_PC\zxyv_ <zxyv_fujiww@yahoo.co.jp>
2025-06-06 09:33:20 +0900

    ejs及びcssを追加

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const http =require('http');
 const fs =require('fs');
+const ejs =require('ejs');
+const url =require('url');
 
 
-var server =http.createServer(
-    (request,response)=>{
-       fs.readFile('./index.html','utf-8',
-        (err,data)=>{
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.write(data);
-        response.end();
-        });
-    }
-);
+const index_page =fs.readFileSync('./index.ejs','utf-8');
+const other_page =fs.readFileSync('./other.ejs','utf-8');
+const style_css =fs.readFileSync('./style.css','utf-8');
+
+
+var server =http.createServer(getFromClient);
 
 const PORT = 3000; // ポート番号を変数にしておくと便利です
 server.listen(PORT, () => {
@@ -19,3 +17,37 @@ server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
     console.log('サーバーが起動しました。Ctrl+Cで停止できます。');
 });
+
+function getFromClient(req, response) {
+    var url_parts = url.parse(req.url);
+    switch(url_parts.pathname){
+        case '/':
+            var Content =ejs.render(index_page,{
+        title:"Index",
+        content:"これはテンプレート使用したページです。",
+        });
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write(Content);
+        response.end();
+        break;
+        case '/other':
+            var Content =ejs.render(other_page,{
+        title:"Other",
+        content:"これは新しいページです。",
+        });
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write(Content);
+        response.end();
+        break;
+        case '/style.css':
+            response.writeHead(200,{'Content-Type':'text/css'});
+            response.write(style_css);
+            response.end();
+            break;
+        default:
+            response.writeHead(404,{'Content-Type':'text/html'});
+            response.write('<h1>404 Not Found</h1>');
+            response.end();
+            break;
+        }
+}
