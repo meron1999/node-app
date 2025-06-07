6d0c40b0e934147213fc54cb02f9a10b0ee7d9ad
OTAKE_PC\zxyv_ <zxyv_fujiww@yahoo.co.jp>
2025-06-07 10:17:30 +0900

    フォームからpostされた情報を受け取り表示する。

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const http =require('http');
 const fs =require('fs');
 const ejs =require('ejs');
 const url =require('url');
+const qs =require('querystring');
+
 
 
 const index_page =fs.readFileSync('./index.ejs','utf-8');
@@ -18,27 +20,15 @@ server.listen(PORT, () => {
     console.log('サーバーが起動しました。Ctrl+Cで停止できます。');
 });
 
-function getFromClient(req, response) {
-    var url_parts = url.parse(req.url);
+function getFromClient(request, response) {
+    var url_parts = url.parse(request.url,true);
     switch(url_parts.pathname){
         case '/':
-            var Content =ejs.render(index_page,{
-        title:"Index",
-        content:"これはテンプレート使用したページです。",
-        });
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.write(Content);
-        response.end();
-        break;
+                response_index(request,response,url_parts);
+                break;
         case '/other':
-            var Content =ejs.render(other_page,{
-        title:"Other",
-        content:"これは新しいページです。",
-        });
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.write(Content);
-        response.end();
-        break;
+                response_other(request,response);
+                break;
         case '/style.css':
             response.writeHead(200,{'Content-Type':'text/css'});
             response.write(style_css);
@@ -50,4 +40,49 @@ function getFromClient(req, response) {
             response.end();
             break;
         }
+}
+
+function response_index(request,response,url_parts){
+    var content ="これはIndexページです。";
+            var query =url_parts.query;
+            if(query.msg !=undefined){
+                content +='あなたは、「'+query.msg+'」と送信しました。';
+            }
+            var content =ejs.render(index_page,{
+            title:"Index",
+            content:content,
+            });
+            response.writeHead(200,{'Content-Type':'text/html'});
+            response.write(content);
+            response.end();
+}
+
+function response_other(request,response){
+    var msg ="これはOtherページです。";
+    if (request.method=='POST'){
+        var body ='';
+        request.on('data',function(data){
+            body +=data;
+        });
+        request.on('end',function(){
+            var post =qs.parse(body);
+            msg +='あなたは、「'+post.msg+'」と送信しました。';
+            var content =ejs.render(other_page,{
+            title:"Other",
+            content:msg,
+            });
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write(content);
+        response.end();
+        });
+    }else{ //GETアクセス時の処理
+        var msg ="ページがありません。"
+        var content =ejs.render(other_page,{
+        title:"Other",
+        content:msg,
+        });
+        response.writeHead(200,{'Content-Type':'text/html'});
+        response.write(content);
+        response.end();
+    }
 }
