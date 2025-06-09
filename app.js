d5a197c669233efad96827abb75c4afa3e2f0fb7
OTAKE_PC\zxyv_ <zxyv_fujiww@yahoo.co.jp>
2025-06-09 09:09:45 +0900

    cookieを利用した伝言ページを作成

--- app.js
+++ app.js
@@ -43,11 +43,7 @@ function getFromClient(request, response) {
 }
 
 var data = {
-    'Taro':'090-999-999',
-    'Hanako':'080-888-888',
-    'Sachiko':'070-777-777',
-    'Ichiro':'060-666-666'
-};
+   msg:'no message...'};
 
 var data2 = {
     'Taro':['taro@yamada','090-999-999','Tokyo'],
@@ -58,22 +54,28 @@ var data2 = {
 
 // Indexページのレスポンスを生成する関数
 function response_index(request,response,url_parts){
-            var msg ="これはIndexページです。";
-            var content =ejs.render(index_page,{
-            title:"Index",
-            content:msg,
-            data:data,
-            filename:'data_item',
-            });
-            response.writeHead(200,{'Content-Type':'text/html'});
-            response.write(content);
-            response.end();
+            if(request.method === 'POST'){
+                // POSTリクエストの処理
+                var body = '';
+                request.on('data', function(data) {
+                    body += data;
+                });
+                request.on('end', ()=> {
+                    data = qs.parse(body);
+                    setCookie('msg', data.msg, response);
+                    write_index(request,response);
+                });
+            }
+            else {
+                // GETリクエストの処理
+                write_index(request,response);
+            }
 }
 
 function response_other(request,response){
     var msg ="これはOtherページです。";
-    var content =ejs.render(index_page,{
-        title:"Index",
+    var content =ejs.render(other_page,{
+        title:"Other",
         content:msg,
         data:data2,
         filename:'data_item',
@@ -82,3 +84,35 @@ function response_other(request,response){
     response.write(content);
     response.end();
     };
+
+function write_index(request,response){
+    var msg ="※伝言を表示します。";
+    var cookie_data =getCookie('msg', request);
+    var content =ejs.render(index_page,{
+        title:"Index",
+        content:msg,
+        data:data,
+        cookie_data:cookie_data,
+        });
+    response.writeHead(200,{'Content-Type':'text/html'});
+    response.write(content);
+    response.end();
+}
+
+function setCookie(key, value, response) {
+    var cookie = escape(value);
+    cookie = key + '=' + cookie;
+    response.setHeader('Set-Cookie', [cookie]);
+}
+
+function getCookie(key, request) {
+    var cookie_data = request.headers.cookie != undefined ? request.headers.cookie : '';
+    var data = cookie_data.split(';');
+    for (var i in data){
+        if (data[i].trim().startsWith(key + '=')) {
+            var result = data[i].trim().substring(key.length + 1);
+            return unescape(result);
+        }
+    }
+    return ''; 
+}
